dfe6b17648e9075341a2db08a4697843036ca800
pranavj1001 <pranavj1001@gmail.com>
2016-10-27 20:40:21 +0530

    added the Logging In layout

---
+++ app/src/main/java/com/learning/pranavjain/chattingapplication/LogginIn.java
@@ -0,0 +1,13 @@
+package com.learning.pranavjain.chattingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogginIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logging_in);
+    }
+}
