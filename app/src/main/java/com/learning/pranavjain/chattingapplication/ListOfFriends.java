94ca3ebe0b8ef4c05eabd05c871004dc03d1e98c
pranavj1001 <pranavj1001@gmail.com>
2016-10-27 20:37:48 +0530

    initial commit, added the layout of Friend List

---
+++ app/src/main/java/com/learning/pranavjain/chattingapplication/ListOfFriends.java
@@ -0,0 +1,17 @@
+package com.learning.pranavjain.chattingapplication;
+
+/**
+ * Created by Pranav Jain on 10/27/2016.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListOfFriends extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_of_friends);
+    }
+}
