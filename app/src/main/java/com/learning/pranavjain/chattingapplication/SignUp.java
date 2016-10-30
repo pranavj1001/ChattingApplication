1eeca63b600662072836b5507041c70c129b7478
pranavj1001 <pranavj1001@gmail.com>
2016-10-30 22:55:08 +0530

    added the sign up activity layout

---
+++ app/src/main/java/com/learning/pranavjain/chattingapplication/SignUp.java
@@ -0,0 +1,13 @@
+package com.learning.pranavjain.chattingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
