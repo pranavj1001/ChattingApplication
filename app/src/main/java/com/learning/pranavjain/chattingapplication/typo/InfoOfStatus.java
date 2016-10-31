9f46acc989b19e6ba2a2e19da1fbb8cee56646af
pranavj1001 <pranavj1001@gmail.com>
2016-10-31 12:16:08 +0530

    added InfoOfStatus.java

---
+++ app/src/main/java/com/learning/pranavjain/chattingapplication/typo/InfoOfStatus.java
@@ -0,0 +1,11 @@
+package com.learning.pranavjain.chattingapplication.typo;
+
+/**
+ * Created by Pranav Jain on 10/31/2016.
+ */
+
+public enum InfoOfStatus {
+
+    ONLINE, OFFLINE, UNAPPROVED, INVISIBLE, BUSY
+
+}
