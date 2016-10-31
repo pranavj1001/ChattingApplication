da6b01682cb06ecd42e000053b9ee9bd1741173f
pranavj1001 <pranavj1001@gmail.com>
2016-10-31 12:12:51 +0530

    added InfoOfMessage.java

---
+++ app/src/main/java/com/learning/pranavjain/chattingapplication/typo/InfoOfMessage.java
@@ -0,0 +1,14 @@
+package com.learning.pranavjain.chattingapplication.typo;
+
+/**
+ * Created by Pranav Jain on 10/31/2016.
+ */
+
+public class InfoOfMessage {
+
+    public static final String messageList = "message_list";
+    public static final String userId = "user_id";
+    public static final String sentT = "sendT";
+    public static final String messageText = "message_text";
+
+}
