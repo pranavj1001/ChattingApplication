898bc99820e25cbd653a0da6637f90cf89d41fbd
pranavj1001 <pranavj1001@gmail.com>
2016-10-31 12:09:51 +0530

    added InfoOfFriends.java

---
+++ app/src/main/java/com/learning/pranavjain/chattingapplication/typo/InfoOfFriends.java
@@ -0,0 +1,17 @@
+package com.learning.pranavjain.chattingapplication.typo;
+
+/**
+ * Created by Pranav Jain on 10/31/2016.
+ */
+
+public class InfoOfFriends {
+
+    public static final String friendsList = "friends_list";
+    public static final String userName = "username";
+    public static final String status = "status";
+    public static final String port = "port";
+    public static final String Ip = "Ip";
+    public static final String userKey = "userKey";
+    public static final String message = "message";
+
+}
